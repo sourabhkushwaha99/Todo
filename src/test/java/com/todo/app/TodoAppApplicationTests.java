e2a5b1af631125126c30cb3e0b9efe11a88b047e
Sourabh Kushwaha <sourabhkush99@gmail.com>
2024-07-29 01:30:01 +0530

    configure datasource

--- src/test/java/com/todo/app/TodoAppApplicationTests.java
+++ src/test/java/com/todo/app/TodoAppApplicationTests.java
@@ -1,13 +1,13 @@
 package com.todo.app;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TodoAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
